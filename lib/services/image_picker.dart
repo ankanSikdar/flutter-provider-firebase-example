a4cac26f3662d546ee2025d699e573ad1654c542
Ankan Sikdar <ankansikdar@gmail.com>
2021-09-16 20:09:37 +0530

    Created image picker

---
+++ lib/services/image_picker.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class ImagePickerService {
+  final imagePicker = ImagePicker();
+  // Returns a [File] object pointing to the image that was picked.
+  Future<File> pickImage({required ImageSource source}) async {
+    final pickedFile = await imagePicker.pickImage(source: source);
+    return File(pickedFile!.path);
+  }
+}
