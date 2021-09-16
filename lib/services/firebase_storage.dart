6c3966a2378023a76b690e752049fcda419be719
Ankan Sikdar <ankansikdar@gmail.com>
2021-09-16 19:59:55 +0530

    Created firebase storage

---
+++ lib/services/firebase_storage.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:firebase_provider/services/firestore_path.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseStorageService {
+  FirebaseStorageService({required this.uid}) : assert(uid != null);
+  final String uid;
+
+  /// Upload an avatar from file
+  Future<String> uploadAvatar({
+    required File file,
+  }) async =>
+      await upload(
+        file: file,
+        path: FirestorePath.avatar(uid) + '/avatar.png',
+        contentType: 'image/png',
+      );
+
+  /// Generic file upload for any [path] and [contentType]
+  Future<String> upload({
+    required File file,
+    required String path,
+    required String contentType,
+  }) async {
+    print('uploading to: $path');
+    final storageReference = FirebaseStorage.instance.ref().child(path);
+    final uploadTask = storageReference.putFile(
+        file, SettableMetadata(contentType: contentType));
+
+    final snapshot =
+        await uploadTask.then((TaskSnapshot taskSnapshot) => taskSnapshot);
+
+    if (snapshot.state == TaskState.error) {
+      print('Upload Error ${snapshot.state.toString()}');
+      throw Exception('Upload ERROR');
+    }
+
+    // Url used to download file/image
+    final downloadUrl = await snapshot.ref.getDownloadURL();
+    print('downloadUrl: $downloadUrl');
+    return downloadUrl;
+  }
+}
